190ba09d024a1bf00551872ee00ab8b14190f3fa
gramillos <gonzalo9ramos3@gmail.com>
2024-02-19 15:58:10 +0100

    creacion archivo IU

---
+++ src/iu.js
@@ -0,0 +1,24 @@
+
+
+
+export default class IU extends Phaser.Scene{
+
+    constructor() {
+        super({ key: 'iu' });
+    }
+
+    create(){
+
+
+        this.iniciarInventario();        
+    }
+
+
+    iniciarInventario(){
+
+        
+
+
+    }
+
+}
